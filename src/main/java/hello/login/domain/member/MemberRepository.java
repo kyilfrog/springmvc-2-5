43e6702db8280305b3b68fb1ace767d26de804d8
kyilfrog <160857010+kyilfrog@users.noreply.github.com>
2024-04-21 23:17:07 +0900

    회원가입

---
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,57 @@
+package hello.login.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+	
+	private static Map<Long, Member> store = new HashMap<>();
+	private static Long sequence = 0L;
+	
+	public Member save(Member member) {
+		member.setId(++sequence);
+		log.info("save: member={}", member);
+		store.put(member.getId(), member);
+		return member;
+	}
+	
+	public Member findById(Long id) {
+		return store.get(id);
+	}
+	
+	public Optional<Member> findByLoginId(String loginId) {
+		/*
+		List<Member> all = findAll();
+		for(Member m : all) {
+			if(m.getLoginId().equals(loginId)) {
+				return Optional.of(m);
+			}
+		}
+		return Optional.empty();
+		*/
+		
+		//위의 주석처리한 코드를 람다와 stream을 사용해 동일하게 표현해보았다.
+		return findAll().stream()
+				.filter(m->m.getLoginId().equals(loginId))
+				.findFirst();
+		
+	}
+	
+	public List<Member> findAll() {
+		return new ArrayList<>(store.values());
+	}
+	
+	public void clearStore() {
+		store.clear();
+	}
+}
